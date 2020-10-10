7c534e71ddc936a95ff8373c1846c80ec9b4156d
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-10-10 13:18:01 -0500

    Update setup.sh

--- ML_Models/DeepVision/setup.sh
+++ ML_Models/DeepVision/setup.sh
@@ -18,13 +18,13 @@ cd $FRCN_ROOT/lib
 make
 
 # Caffe Installation.
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
-sudo apt-get install --no-install-recommends libboost-all-dev
-sudo apt-get install libatlas-base-dev
+sudo apt-get install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install -y --no-install-recommends libboost-all-dev
+sudo apt-get install -y libatlas-base-dev
 for req in $(cat caffe-fast-rcnn/python/requirements.txt); do pip install $req; done
 # Fixing common caffe installation issues. (Ref: https://github.com/BVLC/caffe/wiki/Commonly-encountered-build-issues)
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev 
-sudo apt-get uninstall libatlas-base-dev
+sudo apt-get install -y libgflags-dev libgoogle-glog-dev liblmdb-dev 
+sudo apt-get uninstall -y libatlas-base-dev
 
 # Installing additional python libraries.
 pip install protobuf pyyaml
